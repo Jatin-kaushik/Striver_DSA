8874ae9d034b2bd202770313134d5c63e34a9bbb
Jatin-kaushik <Jatinchandkaushik@gmail.com>
2022-06-20 15:47:58 +0530

    L13 Partially done

---
+++ Tree/L13  Pre In Post in 1 Traversal.py
@@ -0,0 +1,19 @@
+# use pair 1,2,3 to track the visitng nodes
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+a = TreeNode(1)
+a.left = TreeNode(2)   
+a.right = TreeNode(3) 
+a.left.left = TreeNode(4)
+a.left.right = TreeNode(5)
+a.right.left = TreeNode(6)
+a.right.left.right = TreeNode(7)
+a.right.left.right.right = TreeNode(8)
+
+class Solution:
+    def postorderTraversal(self, root):
