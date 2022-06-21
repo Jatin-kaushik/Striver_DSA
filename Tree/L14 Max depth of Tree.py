fd7117f22b5443089c223b4b0145641af8e55934
Jatin-kaushik <Jatinchandkaushik@gmail.com>
2022-06-21 22:27:40 +0530

    L14 max depth

--- Tree/L14 Max depth of Tree.py
+++ Tree/L14 Max depth of Tree.py
@@ -1,6 +1,3 @@
-# use pair 1,2,3 to track the visitng nodes
-
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -14,15 +11,5 @@ a.left.right = TreeNode(5)
 a.right.left = TreeNode(6)
 a.right.left.right = TreeNode(7)
 a.right.left.right.right = TreeNode(8)
-
 class Solution:
     def maxdepth(self, root):
-        if root == None:
-            return 0
-        lh = self.maxdepth(root.left)
-        rh = self.maxdepth(root.right)
-        return 1+ max(lh, rh)
-
-ans = Solution()
-res = ans.maxdepth(a)
-print(res)
